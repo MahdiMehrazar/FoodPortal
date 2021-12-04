2da07a6b88b18d694e34a027453964c3f1ec3172
MahdiMehrazar <mahdi0993@gmail.com>
2021-12-04 17:08:00 +0530

    base commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.status(200).send("Food portal accessed!");
+});
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
