2b733ca70c84819fa1a96cc3efdf777138cc3e07
MahdiMehrazar <mahdi0993@gmail.com>
2021-12-05 16:49:13 +0530

    auth base setup

--- index.js
+++ index.js
@@ -4,11 +4,37 @@
 const express = require("express");
 const path = require("path");
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
 const app = express();
 const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
 
 /**
  *  App Configuration
@@ -17,11 +43,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(expressSession(session));
+
 /**
  * Routes Definitions
  */
 app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+  res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
 /**
@@ -29,5 +61,5 @@ app.get("/", (req, res) => {
  */
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
